33b9b99a403117e8aec37ca787815d2ca8077b0d
fernandodelima <fernando.lima@mercadolibre.com>
2022-07-12 21:43:35 -0300

    Ex1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int num1 = lector.nextInt();
+        System.out.println("Ingrese otro numero");
+        int num2 = lector.nextInt();
+        if (num1 > num2) {
+            System.out.println("El numero mas alto es: " + num1);
+        } else if (num1 < num2) {
+            System.out.println("El numero mas alto es: " + num2);
+        } else {
+            System.out.println("Ambos numeros son iguales");
+        }
     }
 }

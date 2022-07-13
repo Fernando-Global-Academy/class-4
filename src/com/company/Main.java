89bb9467d7de64ce7d153123a5478c7a1269c4af
fernandodelima <fernando.lima@mercadolibre.com>
2022-07-12 22:05:36 -0300

    Menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        ex1();
-        ex2();
+        System.out.println("Seleccione una opcion: ");
+        System.out.println("1) Ex1");
+        System.out.println("2) Ex2");
+        System.out.println("3) Salir");
+        Scanner lector = new Scanner(System.in);
+        int option = lector.nextInt();
+        while (option != 3) {
+            if (option == 1) {
+                ex1();
+            } else if (option == 2) {
+                ex2();
+            } else {
+                System.out.println("Selecciono una opcion no valida");
+            }
+            System.out.println("Seleccione una opcion: ");
+            System.out.println("1) Ex1");
+            System.out.println("2) Ex2");
+            System.out.println("3) Salir");
+            option = lector.nextInt();
+        }
     }
 
     private static void ex2() {

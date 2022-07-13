c63c139ed1492a5c08e936d292eae1b6d4f1fd04
fernandodelima <fernando.lima@mercadolibre.com>
2022-07-12 21:49:47 -0300

    Method ex2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        ex2();
+    }
+
+    private static void ex2() {
         for (int i = 0; i < 10; i++) {
             System.out.println("foo");
         }
